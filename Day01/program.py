451b029524e74b885d892ad10572ab11f68e93dc
Dave Patric <dkpatric@aol.com>
2025-12-02 16:02:45 -0500

    passed part 1

--- Day01/program.py
+++ Day01/program.py
@@ -1,10 +1,30 @@
+# starting location for dial
+dial = 50
+count = 0
 
-def main(filename):
-    with open(filename) as file:
-        for line in file:
-            print(line.strip())
-    # starting location for dial
-    dial = 50
-    print("testing")
+print(f"initial dial: {dial}\n")
 
-main("demofile.txt")
+with open("realdata.txt") as file:
+    for line in file:
+        data = line.strip()
+        direction = data[0]
+        value = int(data[1:])
+
+        if direction == "R":
+            dial += value
+            if dial > 99:
+                dial = dial % 100
+            # print(f"data: {data} -- dial: {dial}")
+        elif direction == "L":
+            dial -= value
+            if dial < 0:
+                dial = dial % 100
+            # print(f"data: {data} -- dial: {dial}")
+        else:
+            print("*** invalid direction ***")
+            SystemExit
+
+        if dial == 0:
+            count += 1
+
+print(f"count: {count}")
