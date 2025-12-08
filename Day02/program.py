6527951fc8a4acc92dcb1ff9c997892fc73ce2ab
Dave Patric <dkpatric@aol.com>
2025-12-08 08:37:58 -0500

    Finished Day02 Part1

--- Day02/program.py
+++ Day02/program.py
@@ -1,14 +1,34 @@
-
 # Day02
 
-with open("Day02/testdata.txt") as file:
-# with open("Day02/realdata.txt") as file:
-    line = file.read()
-    ranges = line.split(",")
-    for range in ranges:
-        limit = range.split("-")
-        start = limit[0]
-        stop = limit[1]
-        print(f"range to check: {range}")
-        print(f"start: {start}")
-        print(f"stop:  {stop}")
+def find_repeats(number):
+    num_str = str(number)
+    mid = len(num_str) // 2
+    if num_str[0:mid] == num_str[mid:]:
+        # print(f"*** repeated: {number}")
+        return True
+    else:
+        return False
+    
+def process_file(filename):
+    sum = 0
+
+    with open(filename) as file:
+    # with open("Day02/realdata.txt") as file:
+        line = file.read()
+        groups = line.split(",")
+        for group in groups:
+            limit = group.split("-")
+            start = int(limit[0])
+            stop = int(limit[1]) + 1
+            check = list()
+            for i in range(start, stop + 1):
+                check.append(i)
+            for item in check:
+                if find_repeats(item):
+                    sum += item
+            # break
+
+    return sum
+
+# print(process_file("Day02/testdata.txt"))
+print(process_file("Day02/realdata.txt"))
